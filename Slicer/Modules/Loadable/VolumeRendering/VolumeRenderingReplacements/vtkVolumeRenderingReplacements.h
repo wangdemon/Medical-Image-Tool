920ba600049e841f9971283a6cca9ee5034b73d3
wangdemon <wangdemon@foxmail.com>
2018-05-20 17:05:05 +0800

    Delete vtkVolumeRenderingReplacements.h

--- Slicer/Modules/Loadable/VolumeRendering/VolumeRenderingReplacements/vtkVolumeRenderingReplacements.h
+++
@@ -1,8 +0,0 @@
-/// vtkVolumeRenderingReplacements.h
-
-#ifndef __vtkVolumeRenderingReplacements_h
-#define __vtkVolumeRenderingReplacements_h
-
-#define VTK_VOLUMERENDERINGREPLACEMENTS_EXPORT
-
-#endif // __vtkVolumeRenderingReplacements_h
